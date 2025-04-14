935c6940eec6718dd4260bd34bac33df2047aae1
kietta <truonganhkietdev@gmail.com>
2025-04-14 11:58:22 +0700

    add UnitOfWork

---
+++ MilkteaShop-BE/DAL/Repositories/Implements/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using DAL.Context;
+using DAL.Repositories.Interfaces;
+
+namespace DAL.Repositories.Implements
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
---
+++ MilkteaShop-BE/DAL/Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace DAL.Repositories.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        Task SaveAsync();
+    }
+}
