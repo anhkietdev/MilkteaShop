63431541d5774af272fa1831c9bed52484b524f2
kietta <truonganhkietdev@gmail.com>
2025-04-14 19:15:10 +0700

    add migration

---
+++ MilkteaShop-BE/DAL/Migrations/20250414121147_refactorProductAndOrder.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class refactorProductAndOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Toppings_Products_ProductId",
+                table: "Toppings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Toppings_ProductId",
+                table: "Toppings");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "Toppings");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "ProductVariants");
+
+            migrationBuilder.RenameColumn(
+                name: "TotalPrice",
+                table: "Orders",
+                newName: "TotalAmount");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Toppings",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CompletedAt",
+                table: "Orders",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Note",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OrderNumber",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompletedAt",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Note",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "OrderNumber",
+                table: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "TotalAmount",
+                table: "Orders",
+                newName: "TotalPrice");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Toppings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProductId",
+                table: "Toppings",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "ProductVariants",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Toppings_ProductId",
+                table: "Toppings",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Toppings_Products_ProductId",
+                table: "Toppings",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id");
+        }
+    }
+}
