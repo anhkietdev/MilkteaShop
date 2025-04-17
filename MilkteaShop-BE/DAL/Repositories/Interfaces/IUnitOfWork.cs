425c0463815801e4c0f633044a3e526db1417d5c
kietta <truonganhkietdev@gmail.com>
2025-04-18 00:44:25 +0700

    add IRepo(s) to IUnitOfWork

--- MilkteaShop-BE/DAL/Repositories/Interfaces/IUnitOfWork.cs
+++ MilkteaShop-BE/DAL/Repositories/Interfaces/IUnitOfWork.cs
@@ -2,6 +2,17 @@
 {
     public interface IUnitOfWork
     {
+        public ICategoryRepository CategoryRepository { get; }
+        public IProductRepository ProductRepository { get; }
+        public IComboRepository ComboRepository { get; }
+        public IComboItemRepository ComboItemRepository { get; }
+        public IOrderRepository OrderRepository { get; }
+        public IOrderItemRepository OrderItemRepository { get; }
+        public IOrderItemToppingRepository OrderItemToppingRepository { get; }
+        public IProductVariantRepository ProductVariantRepository { get; }
+        public IProductVariantToppingRepository ProductVariantToppingRepository { get; }
+        public ISizeRepository SizeRepository { get; }
+        public IToppingRepository ToppingRepository { get; }
         Task SaveAsync();
     }
 }
