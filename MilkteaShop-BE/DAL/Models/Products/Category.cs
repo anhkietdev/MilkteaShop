16f72a52b410444437a6912c760a407d7a5f1da9
leduc <leducnam2003hl@gmail.com>
2025-04-18 16:10:29 +0700

    add database

--- MilkteaShop-BE/DAL/Models/Products/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DAL.Models.Products
-{
-    public class Category : BaseEntity
-    {
-        public required string Name { get; set; }
-        public required string Description { get; set; }
-        public virtual ICollection<Product> Products { get; set; }
-    }
-}
--- MilkteaShop-BE/DAL/Models/Products/Size.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DAL.Models.Products
-{
-    public class Size : BaseEntity
-    {
-        public required string Name { get; set; } // S, M, L, XL...
-        public required int VolumeInMl { get; set; } // 300, 500, 700...
-        public virtual ICollection<ProductVariant> ProductVariants { get; set; }
-    }
-}
