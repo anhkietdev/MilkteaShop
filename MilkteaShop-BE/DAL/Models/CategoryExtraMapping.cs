d1d4db0f9a9dd429e9845df9a073eda257293022
kietta <truonganhkietdev@gmail.com>
2025-04-19 23:58:55 +0700

    update categoryExtraMapping

--- MilkteaShop-BE/DAL/Models/CategoryExtraMapping.cs
+++ MilkteaShop-BE/DAL/Models/CategoryExtraMapping.cs
@@ -2,7 +2,9 @@
 {
     public class CategoryExtraMapping : BaseEntity
     {
-        public Guid CategoryId { get; set; }
-        public required virtual Category Category { get; set; }
+        public Guid MainCategoryId { get; set; }
+        public virtual Category MainCategory { get; set; }
+        public Guid ExtraCategoryId { get; set; }
+        public virtual Category ExtraCategory { get; set; }
     }
 }
