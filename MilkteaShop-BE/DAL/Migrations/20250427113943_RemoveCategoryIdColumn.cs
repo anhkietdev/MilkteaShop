ba3581dc8b410087ba7c9fa7105168831379cd2e
kietta <truonganhkietdev@gmail.com>
2025-04-27 18:41:42 +0700

    remove CategoryId in CategoryExtraMappings in db

---
+++ MilkteaShop-BE/DAL/Migrations/20250427113943_RemoveCategoryIdColumn.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveCategoryIdColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // First drop the foreign key constraint
+            migrationBuilder.DropForeignKey(
+                name: "FK_CategoryExtraMappings_Categories_CategoryId",
+                table: "CategoryExtraMappings");
+
+            // Then drop the index
+            migrationBuilder.DropIndex(
+                name: "IX_CategoryExtraMappings_CategoryId",
+                table: "CategoryExtraMappings");
+
+            // Finally drop the column
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "CategoryExtraMappings");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
