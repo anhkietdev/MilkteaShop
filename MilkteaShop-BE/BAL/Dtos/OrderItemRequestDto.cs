f47cdd376590b46929ff10d9dec2b368746ceeec
kietta <truonganhkietdev@gmail.com>
2025-05-01 14:30:30 +0700

    refactor OrderItemRequestDto

--- MilkteaShop-BE/BAL/Dtos/OrderItemRequestDto.cs
+++ MilkteaShop-BE/BAL/Dtos/OrderItemRequestDto.cs
@@ -5,14 +5,11 @@ namespace BAL.Dtos
     public class OrderItemRequestDto
     {
         public Guid OrderId { get; set; }
-        public required virtual Order Order { get; set; }
         public Guid ProductSizeId { get; set; }
-        public required virtual ProductSize ProductSize { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
         public string? Description { get; set; }
         public Guid? ParentOrderItemId { get; set; }
-        public virtual OrderItem? ParentOrderItem { get; set; }
         public virtual ICollection<OrderItem> ToppingItems { get; set; } = new List<OrderItem>();
     }
 }
