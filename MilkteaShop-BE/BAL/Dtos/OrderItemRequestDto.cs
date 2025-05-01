3c441d16a1de5c70635af4b15443b900b56f2991
kietta <truonganhkietdev@gmail.com>
2025-05-01 16:31:14 +0700

    refactor price

--- MilkteaShop-BE/BAL/Dtos/OrderItemRequestDto.cs
+++ MilkteaShop-BE/BAL/Dtos/OrderItemRequestDto.cs
@@ -7,7 +7,6 @@ namespace BAL.Dtos
         public Guid OrderId { get; set; }
         public Guid ProductSizeId { get; set; }
         public int Quantity { get; set; }
-        public decimal Price { get; set; }
         public string? Description { get; set; }
         public Guid? ParentOrderItemId { get; set; }
         public virtual ICollection<OrderItem>? ToppingItems { get; set; } 
