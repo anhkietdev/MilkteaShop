7df80ade8e46d46be93c592a3a9d0c637a9eda90
kietta <truonganhkietdev@gmail.com>
2025-05-02 14:49:42 +0700

    refactor OrderItemRequestDto

--- MilkteaShop-BE/BAL/Dtos/OrderItemRequestDto.cs
+++ MilkteaShop-BE/BAL/Dtos/OrderItemRequestDto.cs
@@ -6,6 +6,6 @@
         public Guid ProductSizeId { get; set; }
         public int Quantity { get; set; }
         public string? Description { get; set; }
-        public virtual ICollection<ProductSizeRequestDto>? ToppingItems { get; set; } 
+        public virtual ICollection<Guid>? ToppingItems { get; set; } 
     }
 }
