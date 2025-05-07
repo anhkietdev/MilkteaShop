19d78837e09a2bdb5bf294d09beebc3ef07fcde5
leduc <leducnam2003hl@gmail.com>
2025-05-07 18:03:38 +0700

    Gettop5 Product and CreateCombo

---
+++ MilkteaShop-BE/DAL/Models/TopSellingProductDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class TopSellingProductDto
+    {
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int TotalQuantitySold { get; set; }
+        public string? ImageUrl { get; set; }
+        public Guid CategoryId { get; set; }
+
+    }
+
+}
