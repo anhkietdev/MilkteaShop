022b72e37c37ba61f7e1280c373c35547beaabdc
leduc <leducnam2003hl@gmail.com>
2025-05-09 13:29:43 +0700

    Fix data response of Size is Size

--- MilkteaShop-BE/BAL/Dtos/ComboProductSizeResponseDto.cs
+++ MilkteaShop-BE/BAL/Dtos/ComboProductSizeResponseDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,7 @@ namespace BAL.Dtos
     public class ComboProductSizeResponseDto
     {
         public Guid ProductSizeId { get; set; }
-        public string Size { get; set; }
+        public Size? Size { get; set; }
         public decimal Price { get; set; }
     }
 }
